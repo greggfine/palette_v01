db900bf2eb0cc1222c30dc9d1c3d72e0c45ac338
Gregg Fine <bbeez8500@gmail.com>
2018-01-26 14:51:43 -0500

    Added instrument input form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('connected');
+const instInputForm = document.querySelector("#inst-input-form");
+let instInput = document.querySelector("#instrument");
+
+const instrumentList = document.querySelector(".collection");
+
+instInputForm.addEventListener("submit",(e) => {
+	e.preventDefault();
+	let instListItem = document.createElement("li");
+	instListItem.textContent = e.target[0].value;
+	instListItem.className = "collection-item";
+	instrumentList.appendChild(instListItem);
+	instInput.value = "";
+})
